e4dfd07e7a682c5c30bfa17c563ede7ba964a248
AALANZR\alan <alan.reino@aluno.positivo.edu.br>
2018-04-09 00:28:44 -0300

    Commit Master

---
+++ src/main/java/br/edu/up/divergentes/ecommerce/EcoomerceApplication.java
@@ -0,0 +1,12 @@
+package br.edu.up.divergentes.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoomerceApplication.class, args);
+	}
+}
