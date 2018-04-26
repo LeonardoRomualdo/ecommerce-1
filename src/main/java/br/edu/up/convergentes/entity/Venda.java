3d13f868dafeae3a0400f6058f9f325f63605783
LeonardoRomualdo <leonardo.rommualdo@hotmail.com>
2018-04-25 23:23:54 -0300

    Foi adicionado a entidade Venda, assim como interface, repository e controller

--- src/main/java/br/edu/up/convergentes/entity/Venda.java
+++ src/main/java/br/edu/up/convergentes/entity/Venda.java
@@ -2,11 +2,13 @@ package br.edu.up.convergentes.entity;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Digits;
 
 @Entity
 @Table(name="venda")
@@ -16,18 +18,24 @@ public class Venda {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long Id;
 	
+	@Column(name="id_produto")
 	private Long id_produto;
 	
+	@Column(name="id_cliente")
 	private Long id_cliente;
 	
+	@Column(name="valor_desconto")
+	@Digits(integer=3, fraction=2)
 	private BigDecimal desconto;
 	
+	@Column(name="valor_total")
+	@Digits(integer=3, fraction=2)
 	private BigDecimal valorTotal;
-
+	
 	public Venda() {
 	}
 
-	public Venda(Long id, Long id_produto, Long id_cliente, BigDecimal desconto, BigDecimal valorTotal) {
+	public Venda(Long id, Long id_produto, Long id_cliente, @Digits(integer=3, fraction=2) BigDecimal desconto, @Digits(integer=3, fraction=2) BigDecimal valorTotal) {
 		super();
 		Id = id;
 		this.id_produto = id_produto;
@@ -36,7 +44,7 @@ public class Venda {
 		this.valorTotal = valorTotal;
 	}
 
-	public Venda(Long id_produto, Long id_cliente, BigDecimal desconto, BigDecimal valorTotal) {
+	public Venda(Long id_produto, Long id_cliente, @Digits(integer=3, fraction=2) BigDecimal desconto, @Digits(integer=3, fraction=2) BigDecimal valorTotal) {
 		super();
 		this.id_produto = id_produto;
 		this.id_cliente = id_cliente;
